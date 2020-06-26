ca3aea60f63f9fbfe4761e00c98e99bfb424fa8b
larrykim <foflzla12@gmail.com>
2020-06-26 15:25:37 +0900

    item02-1 점층적 생성자 패턴

---
+++ src/com/larrykim/Main.java
@@ -0,0 +1,9 @@
+package com.larrykim;
+
+public class Main {
+
+    public static void main(String[] args) {
+        NutritionFacts nutritionFacts = new NutritionFacts(10,100);
+        System.out.println(nutritionFacts.toString());
+    }
+}
